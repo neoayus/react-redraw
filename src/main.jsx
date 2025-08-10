61ac45e5dd2bbd10c9522680d3958f201599c8b0
neoayus <ayus43@outlook.com>
2025-08-10 16:15:46 +0530

    chore: add dir for Styles and custom style to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 // import './index.css'
 
+// add external (maybe-temp) stylesheet
+import './StyleSheet/main.css'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
