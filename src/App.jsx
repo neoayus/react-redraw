281ee742df2abbbc8b41ff95ebcfaeb233bfc857
neoayus <ayus43@outlook.com>
2025-08-10 17:29:23 +0530

    feat: add notificatio-bar component w 'clock' and 'status icon'

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ import Clock from './Components/Clock.jsx'
 export default function App() {
   return (
     <>
-      {/* <h1> THis is App.jsx</h1> */}
-      <Clock/>
+      <NotificatoinBar/>
     </>
   )
 }
