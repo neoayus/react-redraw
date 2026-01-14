49ddeb6eb05f5cf8edcee759ab4cadc173e33169
neoayus <ayus43@outlook.com>
2026-01-15 00:00:04 +0530

    component: Pill (for capsule shahped buttons)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react' ; 
-import NotificatoinBar from './Components/NotificationBar.jsx' ;
-import HeaderPanel from './Components/HeaderPanel.jsx';
+// import NotificatoinBar from './Components/NotificationBar.jsx' ;
+// import Pill from './Components/Pill.jsx'
 
 export default function App() {
   return (
